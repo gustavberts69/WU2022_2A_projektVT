67b6a223b60ecf377c0b00465ef56a154ef50e1d
gustavberts69 <gustav.berts@elev.ga.ntig.se>
2022-02-21 16:55:37 +0100

    eh

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("nyhet_bild");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+
+
+
+
+function openMenu() {
+  document.getElementById("menu_content").style.height = "87%";
+}
+
+function closeMenu() { 
+  document.getElementById("menu_content").style.height = "0%";
+}
